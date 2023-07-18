288c6fd1940b61c6e2675d337afbab3a6e5a8d92
cyejing <chenyejing@didiglobal.com>
2023-07-18 16:56:04 +0800

    test: add cert test

--- tests/socks.rs
+++ tests/socks.rs
@@ -9,7 +9,7 @@ async fn test_socks() {
 
     common::start_web_server().await;
     common::start_server("tests/examples/shuttles.yaml").await;
-    common::start_client("tests/examples/shuttlec.yaml").await;
+    common::start_client("tests/examples/shuttlec-proxy.yaml").await;
 
     let client = reqwest::Client::builder()
         .proxy(reqwest::Proxy::http("socks5://127.0.0.1:4082").unwrap())
